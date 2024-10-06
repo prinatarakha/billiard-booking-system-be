1b033fe91ec41cb399ee8952c9430c72a39107c2
prinatarakha <prinatarakha@gmail.com>
2024-10-07 02:07:07 +0700

    feat: get waiting list entries api

--- src/waitingList/waitingList.dto.ts
+++ src/waitingList/waitingList.dto.ts
@@ -25,3 +25,43 @@ export type WaitingListEntryResponse = {
   created_at: Date,
   updated_at: Date,
 }
+
+export const GetWaitingListEntriesRequest = z.object({
+  query: z.object({
+    page: z
+      .string()
+      .transform((val) => parseInt(val, 10))
+      .refine((val) => !isNaN(val) && val > 0, {
+        message: `Page must be a positive integer value`
+      })
+      .optional(),
+    limit: z
+      .string()
+      .transform((val) => parseInt(val, 10))
+      .refine((val) => !isNaN(val) && val > 0, {
+        message: `Limit must be a positive integer value`
+      })
+      .optional(),
+    table_id: z.string().uuid().optional(),
+    statuses: z
+      .string()
+      .refine((val) => {
+        const statuses = val.split(",");
+        const waitingListStatuses = Object.values(Constants.WaitingListStatusEnums);
+        const isNotValid = statuses.some((status) => {
+          const cleanedStatus = status.charAt(0) === "!" ? status.slice(1) : status; // "!" means NOT operator
+          return !waitingListStatuses.includes(cleanedStatus)
+        });
+        return !isNotValid;
+      }, {
+        message: `Invalid waiting list status!`
+      })
+      .optional(),
+  })
+});
+
+export type GetWaitingListEntriesResponse = PaginationResponse & {
+  table_id: string | null,
+  statuses: string[],
+  waiting_list_entries: WaitingListEntryResponse[],
+}
