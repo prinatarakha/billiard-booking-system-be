2aba286506682328be47b03e7d1d90be38950182
prinatarakha <prinatarakha@gmail.com>
2024-10-15 00:49:26 +0700

    feat: fulfill waiting list entry api

--- src/waitingList/waitingList.routes.ts
+++ src/waitingList/waitingList.routes.ts
@@ -11,4 +11,10 @@ router.get("/v1/waiting-list/:id", validateRequest(DTO.GetWaitingListEntryReques
 router.delete("/v1/waiting-list/:id", validateRequest(DTO.DeleteWaitingListEntryRequest), Handlers.deleteWaitingListEntry); // not recommended. Just update the status to cancelled
 router.put("/v1/waiting-list/:id", validateRequest(DTO.UpdateWaitingListEntryRequest), Handlers.updateWaitingListEntry);
 
+// NOTES: Fulfilling a waiting list entry means creating a new table occupation. Although creating new table occupation has its own API,
+//        it is done in this module for avoiding circular dependency. Because, if we add 'waiting_list_id' in req body of create table occupation 
+//        API, then we need to check the existence of the waiting list entry, which involves importing the waiting list module in table
+//        occupation module, resulting in circular dependency because table occupation module is already imported here.
+router.post("/v1/waiting-list/:id/fulfill", validateRequest(DTO.FulfillWaitingListEntryRequest), Handlers.fulfillWaitingListEntry);
+
 export default router;
